ef74c00a1aae7f42a63df81ce0c22287724eeefc
Zac Copeland <zcopeland87@gmail.com>
2017-09-02 04:27:54 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '0Y3iiJIgVzQRekPPR8udG4z4H',
+  consumer_secret: 'DH9cdwObUnSAIapdigfXFB7rP2hI2ZLCoL42eNUaCTYLNtSmOw',
+  access_token_key: '0kBy1rNW6XSD44vlcdZ3d28rAafPgtyFE9yi6ia',
+  access_token_secret: 'YHa5FzquwolJfVlUCGMkklKOV1lh5ZwarpynaZlscZt4Q',
+}
+
+exports.spotifyKeys = {
+	id: '805e5b2ce8af450e834a3c14b684bdb5',
+	secret: '5ef16f71de4d417f95a5b6ae4eb35fea'
+}
+
+exports.omdbKey = {
+	key: '40e9cece'
+}
